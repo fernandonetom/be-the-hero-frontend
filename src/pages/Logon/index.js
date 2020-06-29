aad482945fc85e07a0598c5c5147a9dc6df22f86
fernando39357 <fernando39357@gmail.com>
2020-06-29 12:08:33 -0300

    Logo and resposive

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { FiLogIn } from "react-icons/fi";
 import "./styles.css";
 import heroesImg from "../../assets/heroes.png";
-import logoImg from "../../assets/logo.svg";
+import logoImg from "../../assets/logo.png";
 
 export default function Logon() {
 	return (
