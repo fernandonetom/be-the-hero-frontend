8c50df6c4a5c85f266643680ce3a4846b7d975c0
fernando39357 <fernando39357@gmail.com>
2020-06-29 20:00:35 -0300

    Profile page + styles

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,14 +1,114 @@
 import React from "react";
 import "./styles.css";
 import { Link } from "react-router-dom";
-import { FiArrowLeft } from "react-icons/fi";
+import { FiPower, FiTrash2 } from "react-icons/fi";
 
 import logoImg from "../../assets/logo.png";
-
 export default function Profile() {
 	return (
 		<div className="profile-container">
-			<h1>Listar os casos de uma ong</h1>
+			<header>
+				<img src={logoImg} alt="Apoie uma ONG" />
+				<span>Bem vinda, amigos de quatro patas</span>
+
+				<Link className="button" to="/incidents/new">
+					Cadastrar novo caso
+				</Link>
+				<button type="button">
+					<FiPower size={18} color="#e02041" />
+				</button>
+			</header>
+			<h1>Casos cadastrados</h1>
+
+			<ul>
+				<li>
+					<div className="img-cover">
+						<img src="http://lorempixel.com/500/200 " alt="" />
+					</div>
+					<div className="item-body">
+						<h3>Cadela abandonada</h3>
+
+						<strong>DESCRIÇÃO</strong>
+						<p>
+							Descrição teste Descrição teste Descrição teste Descrição teste
+							Descrição teste Descrição teste Descrição teste
+						</p>
+
+						<strong>VALOR:</strong>
+						<p>R$ 120,00</p>
+
+						<button type="button" title="Excluir caso">
+							<FiTrash2 size={20} color="#a8a8b3" />
+						</button>
+					</div>
+				</li>
+
+				<li>
+					<div className="img-cover">
+						<img src="http://lorempixel.com/500/200 " alt="" />
+					</div>
+					<div className="item-body">
+						<h3>Cadela abandonada</h3>
+
+						<strong>DESCRIÇÃO</strong>
+						<p>
+							Descrição teste Descrição teste Descrição teste Descrição teste
+							Descrição teste Descrição teste Descrição teste
+						</p>
+
+						<strong>VALOR:</strong>
+						<p>R$ 120,00</p>
+
+						<button type="button" title="Excluir caso">
+							<FiTrash2 size={20} color="#a8a8b3" />
+						</button>
+					</div>
+				</li>
+
+				<li>
+					<div className="img-cover">
+						<img src="http://lorempixel.com/500/200 " alt="" />
+					</div>
+					<div className="item-body">
+						<h3>Cadela abandonada</h3>
+
+						<strong>DESCRIÇÃO</strong>
+						<p>
+							Descrição teste Descrição teste Descrição teste Descrição teste
+							Descrição teste Descrição teste Descrição teste
+						</p>
+
+						<strong>VALOR:</strong>
+						<p>R$ 120,00</p>
+
+						<button type="button" title="Excluir caso">
+							<FiTrash2 size={20} color="#a8a8b3" />
+						</button>
+					</div>
+				</li>
+
+				<li>
+					<div className="img-cover">
+						<img src="http://lorempixel.com/500/200 " alt="" />
+					</div>
+					<div className="item-body">
+						<h3>Cadela abandonada</h3>
+
+						<strong>DESCRIÇÃO</strong>
+						<p>
+							Descrição teste Descrição teste Descrição teste Descrição teste
+							Descrição teste Descrição teste Descrição teste
+						</p>
+
+						<strong>VALOR:</strong>
+						<p>R$ 120,00</p>
+
+						<button type="button" title="Excluir caso">
+							<FiTrash2 size={20} color="#a8a8b3" />
+						</button>
+					</div>
+				</li>
+			</ul>
 		</div>
 	);
 }
