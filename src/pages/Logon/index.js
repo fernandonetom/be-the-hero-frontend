493332a92a2ddf3a70072b517e3e9868bf0e8679
fernando39357 <fernando39357@gmail.com>
2020-06-28 22:10:51 -0300

    Login page styles

---
+++ src/pages/Logon/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { FiLogIn } from "react-icons/fi";
+import "./styles.css";
+import heroesImg from "../../assets/heroes.png";
+import logoImg from "../../assets/logo.svg";
+
+export default function Logon() {
+	return (
+		<div className="logon-container">
+			<section className="form">
+				<img className="logo" src={logoImg} alt="Logo" />
+				<form>
+					<h1>Faça seu login</h1>
+
+					<input type="text" placeholder="Sua ID" />
+					<button className="button" type="submit">
+						Entrar
+					</button>
+
+					<a href="/register">
+						<FiLogIn siz={16} color="#E02041" />
+						Não tenho cadastro
+					</a>
+				</form>
+			</section>
+			<img className="pessoas" src={heroesImg} alt="Heroes" />
+		</div>
+	);
+}
